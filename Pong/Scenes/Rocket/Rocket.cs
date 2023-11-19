af46d4f4134a3e3b786c85fad5c2b740216293e9
ukrgerri4 <ukrgerri4@gmail.com>
2023-11-19 15:32:15 +0200

    add background shader

--- Pong/Scenes/Rocket/Rocket.cs
+++ Pong/Scenes/Rocket/Rocket.cs
@@ -3,67 +3,67 @@ using System;
 
 public partial class Rocket : CharacterBody2D
 {
-    // WHO launched?
+	// WHO launched?
 
-    public Vector2 _velocity = Vector2.Zero;
-    public float _speed = 450f;
-    private float _rotationMultiplier = 3f;
-    private float _minRotationAngle = Mathf.Pi / 6;
-    private PlayerInputManager _inputManager;
+	public Vector2 _velocity = Vector2.Zero;
+	public float _speed = 370f;
+	private float _rotationMultiplier = 3f;
+	private float _minRotationAngle = Mathf.Pi / 6;
+	private PlayerInputManager _inputManager;
 
-    // public delegate void AttackedEventHandler();
-    // public AttackedEventHandler Destroyed;
+	// public delegate void AttackedEventHandler();
+	// public AttackedEventHandler Destroyed;
 
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 
-    }
+	}
 
-    public void Init(PlayerInputManager inputManager, Vector2 baseVelocity)
-    {
-        _inputManager = inputManager;
-        _velocity = baseVelocity;
-    }
+	public void Init(PlayerInputManager inputManager, Vector2 baseVelocity)
+	{
+		_inputManager = inputManager;
+		_velocity = baseVelocity;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        _velocity = GetVelocity(delta);
+	public override void _PhysicsProcess(double delta)
+	{
+		_velocity = GetVelocity(delta);
 
-        var collision = MoveAndCollide(_velocity.Normalized() * _speed * (float)delta);
+		var collision = MoveAndCollide(_velocity.Normalized() * _speed * (float)delta);
 
-        if (collision is not null)
-        {
-            var collider = collision.GetCollider();
+		if (collision is not null)
+		{
+			var collider = collision.GetCollider();
 
-            QueueFree();
-            return;
-        }
+			QueueFree();
+			return;
+		}
 
-        LookAt(GlobalPosition + _velocity.Normalized());
-    }
+		LookAt(GlobalPosition + _velocity.Normalized());
+	}
 
-    private Vector2 GetVelocity(double delta)
-    {
-        var moveDirection = new Vector2(
-            _inputManager.GetRightXStrength(),
-            _inputManager.GetRightYStrength()
-        );
+	private Vector2 GetVelocity(double delta)
+	{
+		var moveDirection = new Vector2(
+			_inputManager.GetRightXStrength(),
+			_inputManager.GetRightYStrength()
+		);
 
-        if (moveDirection.LengthSquared() < 0.15f)
-        {
-            return _velocity;
-        }
+		if (moveDirection.LengthSquared() < 0.15f)
+		{
+			return _velocity;
+		}
 
-        var rotationAngle = _velocity.AngleTo(moveDirection);
+		var rotationAngle = _velocity.AngleTo(moveDirection);
 
-        // TODO: refactoring
-        var angle = Mathf.Abs(rotationAngle) >= _minRotationAngle
-            ? rotationAngle
-            : rotationAngle >= 0
-                ? _minRotationAngle
-                : _minRotationAngle * -1;
+		// TODO: refactoring
+		var angle = Mathf.Abs(rotationAngle) >= _minRotationAngle
+			? rotationAngle
+			: rotationAngle >= 0
+				? _minRotationAngle
+				: _minRotationAngle * -1;
 
-        return _velocity.Rotated(angle * (float)delta * _rotationMultiplier);
-    }
+		return _velocity.Rotated(angle * (float)delta * _rotationMultiplier);
+	}
 }
